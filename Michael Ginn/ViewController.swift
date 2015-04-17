fce625fa9c5703386c6444a12b9eae768ae996bd
Michael Ginn <milesper@Michaels-MacBook-Pro.local>
2015-04-17 12:40:18 -0500

    Initial Commit

---
+++ Michael Ginn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Michael Ginn
+//
+//  Created by Michael Ginn on 4/17/15.
+//  Copyright (c) 2015 Michael Ginn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
